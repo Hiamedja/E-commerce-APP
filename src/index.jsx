1528e229d74e7fa0efd5c59d5280aeff0fea3e38
Hiba AMEDJAR <117330946+Hiamedja@users.noreply.github.com>
2025-04-17 12:30:36 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,17 @@
-// src/main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import './index.css';
-// import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import './main.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter> */}
+
       <CartProvider>
         <App />
       </CartProvider>
-    {/* </BrowserRouter> */}
+
   </React.StrictMode>
 );
